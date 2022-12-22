c5b92cc039715afecc70439c75834dd9575b4445
simionroxanavioleta <simionroxanavioleta@gmail.com>
2022-12-22 23:39:57 +0200

    Laborator8

---
+++ Models/ShopList.cs
@@ -0,0 +1,14 @@
+﻿using SQLite;
+namespace Simion_Roxana_Lab7.Models
+{
+    public class ShopList
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        
+        [MaxLength(250), Unique]
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+
+    }
+}
